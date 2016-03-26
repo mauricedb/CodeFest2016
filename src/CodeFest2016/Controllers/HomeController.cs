2dbd1a815e322268cc9e87ccae7a25d3728803b4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-03-26 09:21:53 +0600

    About screen with runtime info

--- src/CodeFest2016/Controllers/HomeController.cs
+++ src/CodeFest2016/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace CodeFest2016.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRuntimeEnvironment _runtimeEnvironment;
+
+        public HomeController(IRuntimeEnvironment runtimeEnvironment)
+        {
+            _runtimeEnvironment = runtimeEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -15,7 +23,13 @@ namespace CodeFest2016.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+
+            ViewData["Message"] = "Your application is running on:";
+            ViewData["OperatingSystem"] = _runtimeEnvironment.OperatingSystem;
+            ViewData["OperatingSystemVersion"] = _runtimeEnvironment.OperatingSystemVersion;
+            ViewData["RuntimeType"] = _runtimeEnvironment.RuntimeType;
+            ViewData["RuntimeArchitecture"] = _runtimeEnvironment.RuntimeArchitecture;
+            ViewData["RuntimeVersion"] = _runtimeEnvironment.RuntimeVersion;
 
             return View();
         }
